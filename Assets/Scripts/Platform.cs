92330baa8a1e43d82799b27c346f2e9bb26a7ef5
happy1041 <qq1041637640@163.com>
2025-02-09 12:44:57 +0800

    Platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("aaaBullet"))
+        {
+            other.gameObject.SetActive(false);
+            // Debug.Log("Icy!");
+            anim.SetBool("IsIcy",true);
+        }
+    }
+    
+    void IcyExit()
+    {
+        anim.SetBool("IsIcy", false);
     }
 }
