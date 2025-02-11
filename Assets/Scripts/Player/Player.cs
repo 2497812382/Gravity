b2740d882b7d5e4de1bc98261de46a0560c6c313
Aminos <2497812382@qq.com>
2025-02-11 13:09:59 +0800

    结冰检测

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -27,6 +27,8 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private bool isGravity;
     [SerializeField] private LayerMask whatIsGravity;
+    [SerializeField] private bool isIce;
+    [SerializeField] private LayerMask whatIsIce;
     [Space]
     [Header("是否反转操作")]
     [SerializeField] private bool needReverse;
@@ -207,6 +209,7 @@ public class Player : MonoBehaviour
     {
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
         isGravity = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGravity);
+        isIce = hysics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsIce);
     }
     private void OnDrawGizmos()
     {
